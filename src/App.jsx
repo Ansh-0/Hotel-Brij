64f6e0f7dd8ed21722fb09a93ca4d89d53fc3e42
Ansh <anshg1921@gmail.com>
2023-05-10 16:54:23 +0530

    added rooms section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "./app.css";
 import { Cover } from "./components/cover";
+import { Rooms } from "./components/rooms";
 
 function App() {
 	return (
 		<>
 			<Cover />
+			<Rooms />
 		</>
 	);
 }
---
+++ src/components/rooms.jsx
@@ -0,0 +1,27 @@
+export const Rooms = () => {
+	return (
+		<div className='rooms row'>
+			<div className=' room col-12 col-lg-4'>
+				<h1 className='title'>Room 1</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Excepturi, et.
+				</p>
+			</div>
+			<div className='room col-12 col-lg-4'>
+				<h1 className='title'>Room 2</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Excepturi, et.
+				</p>
+			</div>
+			<div className='room col-12 col-lg-4'>
+				<h1 className='title'>Room 3</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Excepturi, et.
+				</p>
+			</div>
+		</div>
+	);
+};
